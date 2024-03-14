5a5094068ec297fe06cf1fcc9d0ec9f4f57c7ca1
Tiejun Gao <terry.tiejun.gao@gmail.com>
2024-03-14 21:40:37 +1100

    restore to original color

--- lua/nord/colors.lua
+++ lua/nord/colors.lua
@@ -1,45 +1,45 @@
 local nord = (vim.o.background == "dark")
-        and {
-            --16 colors
-            nord0_gui = "#2E3440", -- nord0 in palette
-            -- nord1_gui = "#3B4252", -- nord1 in palette
-            nord1_gui = "#20242c", -- nord1 in palette
-            nord2_gui = "#434C5E", -- nord2 in palette
-            nord3_gui = "#4C566A", -- nord3 in palette
-            nord3_gui_bright = "#616E88", -- out of palette
-            nord4_gui = "#D8DEE9", -- nord4 in palette
-            nord5_gui = "#E5E9F0", -- nord5 in palette
-            nord6_gui = "#ECEFF4", -- nord6 in palette
-            nord7_gui = "#8FBCBB", -- nord7 in palette
-            nord8_gui = "#88C0D0", -- nord8 in palette
-            nord9_gui = "#81A1C1", -- nord9 in palette
-            nord10_gui = "#5E81AC", -- nord10 in palette
-            nord11_gui = "#BF616A", -- nord11 in palette
-            nord12_gui = "#D08770", -- nord12 in palette
-            nord13_gui = "#EBCB8B", -- nord13 in palette
-            nord14_gui = "#A3BE8C", -- nord14 in palette
-            nord15_gui = "#B48EAD", -- nord15 in palette
-            none = "NONE",
-        }
+    and {
+        --16 colors
+        nord0_gui = "#2E3440",            -- nord0 in palette
+        nord1_gui = "#3B4252",            -- nord1 in palette
+        -- nord1_gui = "#20242c", -- nord1 in palette
+        nord2_gui = "#434C5E",            -- nord2 in palette
+        nord3_gui = "#4C566A",            -- nord3 in palette
+        nord3_gui_bright = "#616E88",     -- out of palette
+        nord4_gui = "#D8DEE9",            -- nord4 in palette
+        nord5_gui = "#E5E9F0",            -- nord5 in palette
+        nord6_gui = "#ECEFF4",            -- nord6 in palette
+        nord7_gui = "#8FBCBB",            -- nord7 in palette
+        nord8_gui = "#88C0D0",            -- nord8 in palette
+        nord9_gui = "#81A1C1",            -- nord9 in palette
+        nord10_gui = "#5E81AC",           -- nord10 in palette
+        nord11_gui = "#BF616A",           -- nord11 in palette
+        nord12_gui = "#D08770",           -- nord12 in palette
+        nord13_gui = "#EBCB8B",           -- nord13 in palette
+        nord14_gui = "#A3BE8C",           -- nord14 in palette
+        nord15_gui = "#B48EAD",           -- nord15 in palette
+        none = "NONE",
+    }
     or {
         --16 colors
-        nord0_gui = "#ECEFF4", -- nord6 in palette
-        nord1_gui = "#E5E9F0", -- nord5 in palette
-        nord2_gui = "#D8DEE9", -- nord4 in palette
-        nord3_gui = "#4C566A", -- nord3 in palette
+        nord0_gui = "#ECEFF4",        -- nord6 in palette
+        nord1_gui = "#E5E9F0",        -- nord5 in palette
+        nord2_gui = "#D8DEE9",        -- nord4 in palette
+        nord3_gui = "#4C566A",        -- nord3 in palette
         nord3_gui_bright = "#AEC7DF", -- out of palette
-        nord4_gui = "#434C5E", -- nord2 in palette
-        nord5_gui = "#3B4252", -- nord1 in palette
-        nord6_gui = "#2E3440", -- nord0 in palette
-        nord7_gui = "#8FBCBB", -- nord7 in palette
-        nord8_gui = "#88C0D0", -- nord8 in palette
-        nord9_gui = "#81A1C1", -- nord9 in palette
-        nord10_gui = "#5E81AC", -- nord10 in palette
-        nord11_gui = "#BF616A", -- nord11 in palette
-        nord12_gui = "#D08770", -- nord12 in palette
-        nord13_gui = "#EBCB8B", -- nord13 in palette
-        nord14_gui = "#A3BE8C", -- nord14 in palette
-        nord15_gui = "#B48EAD", -- nord15 in palette
+        nord4_gui = "#434C5E",        -- nord2 in palette
+        nord5_gui = "#3B4252",        -- nord1 in palette
+        nord6_gui = "#2E3440",        -- nord0 in palette
+        nord7_gui = "#8FBCBB",        -- nord7 in palette
+        nord8_gui = "#88C0D0",        -- nord8 in palette
+        nord9_gui = "#81A1C1",        -- nord9 in palette
+        nord10_gui = "#5E81AC",       -- nord10 in palette
+        nord11_gui = "#BF616A",       -- nord11 in palette
+        nord12_gui = "#D08770",       -- nord12 in palette
+        nord13_gui = "#EBCB8B",       -- nord13 in palette
+        nord14_gui = "#A3BE8C",       -- nord14 in palette
+        nord15_gui = "#B48EAD",       -- nord15 in palette
         none = "NONE",
     }
 
